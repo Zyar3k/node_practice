cb94dd586d91423033b9ba1144d953bd6242dbdb
zyar3k <a.zygan@gmail.com>
2023-02-22 19:28:44 +0100

    Add ejs template

--- app.js
+++ app.js
@@ -4,14 +4,28 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs");
+
+console.log(app.get("views"));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/index.html"));
+  res.render("index", {
+    title: "Node Practice",
+  });
 });
 app.get("/contact", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/contact.html"));
+  res.render("contact", {
+    title: "Contact Page",
+  });
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/about.html"));
+  res.render("about", {
+    title: "About page",
+  });
+});
+
+app.get("/download", (req, res) => {
+  res.download("./assets/picture.jpg");
 });
 
 app.listen(PORT, () => {
