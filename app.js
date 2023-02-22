a89e3e54eab9021b23233af2540126ec2b148806
zyar3k <a.zygan@gmail.com>
2023-02-22 19:04:28 +0100

    Add  simple routing and nav

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.end("Home page");
+  res.sendFile(path.resolve(__dirname + "/index.html"));
+});
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/contact.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/about.html"));
 });
 
 app.listen(PORT, () => {
