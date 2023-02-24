63e7f95438d167abfbb07af11901a84049c37e29
zyar3k <a.zygan@gmail.com>
2023-02-24 10:24:14 +0100

    Get request from openweather

--- weather_api/app.js
+++ weather_api/app.js
@@ -1,1 +1,18 @@
-console.log("Init");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const https = require("https");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Cracow&lang=pl&appid=${process.env.API_KEY}`;
+  //   res.sendFile(__dirname + "/index.html");
+  https.get(url, (response) => {
+    console.log(response);
+  });
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running: http://localhost:${PORT}`);
+});
