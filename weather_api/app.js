62f700d45e4a055fba7cbc5395b2490ea9de598b
zyar3k <a.zygan@gmail.com>
2023-02-24 11:23:11 +0100

    Close weather api

--- weather_api/app.js
+++ weather_api/app.js
@@ -5,9 +5,20 @@ const https = require("https");
 const app = express();
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Kraków&units=metric&lang=pl&appid=${process.env.API_KEY}`;
-  //   res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  //   console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const units = "metric";
+  const lang = "pl";
+  const apiKey = process.env.API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${units}&lang=${lang}&appid=${apiKey}`;
+
   https.get(url, (response) => {
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
@@ -15,8 +26,9 @@ app.get("/", (req, res) => {
       const weatherIcon = weatherData.weather[0].icon;
       const temp = weatherData.main.temp;
       const imgUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+      res.write(`<h1>Miasto: ${query}</h1>`);
       res.write(`<p>Temperatura: ${temp}</p>`);
-      res.write(`<h1>Opis: ${weatherDesc}</h1>`);
+      res.write(`<h3>Opis: ${weatherDesc}</h3>`);
       res.write(`<img src=${imgUrl} />`);
       res.send();
       //   res.send(
