fda99489ecac54dae52c75a043c0a0855959af11
zyar3k <a.zygan@gmail.com>
2023-02-24 11:05:05 +0100

    Get data from api

--- weather_api/app.js
+++ weather_api/app.js
@@ -6,10 +6,23 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Cracow&lang=pl&appid=${process.env.API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Kraków&units=metric&lang=pl&appid=${process.env.API_KEY}`;
   //   res.sendFile(__dirname + "/index.html");
   https.get(url, (response) => {
-    console.log(response);
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const weatherDesc = weatherData.weather[0].description;
+      const weatherIcon = weatherData.weather[0].icon;
+      const temp = weatherData.main.temp;
+      const imgUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+      res.write(`<p>Temperatura: ${temp}</p>`);
+      res.write(`<h1>Opis: ${weatherDesc}</h1>`);
+      res.write(`<img src=${imgUrl} />`);
+      res.send();
+      //   res.send(
+      //     `<p>Temperatura: ${temp}</p><br/> <h1>Opis: ${weatherDesc}</h1>`
+      //   );
+    });
   });
 });
 
